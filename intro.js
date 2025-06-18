9be6fa2e1472bc2c010acbef374bd71c36854a4b
CaptapultPrincess <jsfglytab@gmail.com>
2025-06-18 09:08:20 -0500

    Animate

--- intro.js
+++ intro.js
@@ -3,11 +3,23 @@ let arrow = document.querySelector(".down-arw");
 let navButtons = document.querySelector(".nav-btn");
 let navbar = document.querySelector(".navbar");
 let navY = 50;
+let time = null;
 
 navbar.style.display = "none";
 arrow.addEventListener('click', function() {
     console.log(navbar.style.display);
-    
+
+function slideDown(){
+    if (navY < 52){  
+        console.log(navY + "px")
+        navbar.style.height = navY + "px";
+        navY = (navY + 1)
+    }else{
+        clearInterval(time);
+    }
+}
+
+
     if (navbar.style.display === "none"){
         navbar.style.display = "block";
         navButtons.style.display = "inline-block";
@@ -15,24 +27,17 @@ arrow.addEventListener('click', function() {
         document.querySelector(".arw-box").style.filter = "opacity(100%)";
         navY = 0;
         navbar.style.height = "0px";
-        //navbar.style.height = "50px";
+        time = setInterval(slideDown/*(1)*/, 4);      
 
-        for(let i = 0; i < 6; i++){
-            setTimeout(function() {
-                console.log(navY + "px")
-                navbar.style.height = navY + "px";
-                navY = (navY + 10)
-                console.log("3 second has passed");    
-            }, 3000);
-            
-    
-        }
     }     
     else{
         navbar.style.display = "none";
         navButtons.style.display = "none";
         arrow.style.transform = "scaleY(1)";
         document.querySelector(".arw-box").style.filter = "opacity(50%)";
+        navY = 50;
+        navbar.style.height = "50px";
+        time = setInterval(slideDown/*(-1)*/, 4); 
 
     }
 });
